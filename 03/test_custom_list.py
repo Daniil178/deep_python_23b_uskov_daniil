3c918b92283816c21a3bb57e73de738a6b55681f
Daniil178 <daniiluskov337@gmail.com>
2023-10-19 22:11:47 +0300

    update tests of add/sub

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -5,6 +5,13 @@ from contextlib import redirect_stdout
 from custom_list import CustomList
 
 
+def compare_clist_list(l1: list, l2: CustomList):
+    for elem_list, elem_clist in zip(l1, l2):
+        if elem_list != elem_clist:
+            return False
+    return True
+
+
 class CustomListTests(TestCase):
 
     def test_init(self):
@@ -14,7 +21,7 @@ class CustomListTests(TestCase):
 
         clist = CustomList([2, 3, 4, 5])
         self.assertEqual(CustomList, type(clist))
-        self.assertEqual([2, 3, 4, 5], clist)
+        self.assertEqual(True, compare_clist_list([2, 3, 4, 5], clist))
 
         clist1 = CustomList(empty)
         self.assertEqual(CustomList, type(clist1))
@@ -33,25 +40,25 @@ class CustomListTests(TestCase):
         res_1 = clist_3 + clist_0
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([1, 2, 3], res_1)
-        self.assertEqual([1, 2, 3], clist_3)
-        self.assertEqual([], clist_0)
+        self.assertEqual(True, compare_clist_list([1, 2, 3], res_1))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
+        self.assertEqual(True, compare_clist_list([], clist_0))
 
         res_2 = clist_0 + clist_3
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([1, 2, 3], res_2)
-        self.assertEqual([1, 2, 3], clist_3)
-        self.assertEqual([], clist_0)
+        self.assertEqual(True, compare_clist_list([1, 2, 3], res_2))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
+        self.assertEqual(True, compare_clist_list([], clist_0))
 
         clist_3_1 = CustomList([3, 2, 1])
         res_3 = clist_3_1 + clist_3
 
         self.assertEqual(CustomList, type(clist_3_1))
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([4, 4, 4], res_3)
-        self.assertEqual([1, 2, 3], clist_3)
-        self.assertEqual([3, 2, 1], clist_3_1)
+        self.assertEqual(True, compare_clist_list([4, 4, 4], res_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
+        self.assertEqual(True, compare_clist_list([3, 2, 1], clist_3_1))
 
     def test_radd_custom_list_and_list(self):
         clist_3 = CustomList([1, 2, 3])
@@ -61,24 +68,24 @@ class CustomListTests(TestCase):
         res_1 = empty + clist_3
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([1, 2, 3], res_1)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([1, 2, 3], res_1))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([], empty)
 
         list_5 = [1, 2, 3, 4, 5]
         res_2 = list_5 + clist_3
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([2, 4, 6, 4, 5], res_2)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([2, 4, 6, 4, 5], res_2))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([1, 2, 3, 4, 5], list_5)
 
         list_3 = [3, 4, 5]
         res_3 = list_3 + clist_3
 
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([4, 6, 8], res_3)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([4, 6, 8], res_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([3, 4, 5], list_3)
 
     def test_add_custom_list_and_list(self):
@@ -91,23 +98,23 @@ class CustomListTests(TestCase):
         res_1 = clist_3 + list_0
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([1, 2, 3], res_1)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([1, 2, 3], res_1))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([], list_0)
 
         res_2 = clist_3 + list_5
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([2, 4, 6, 4, 5], res_2)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([2, 4, 6, 4, 5], res_2))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([1, 2, 3, 4, 5], list_5)
 
         list_3 = [1, 2, 5]
         res_3 = clist_3 + list_3
 
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([2, 4, 8], res_3)
-        self.assertEqual([1, 2, 3], clist_3)
+        self.assertEqual(True, compare_clist_list([2, 4, 8], res_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3))
         self.assertEqual([1, 2, 5], list_3)
 
     def test_sub_only_custom_lists(self):
@@ -120,24 +127,24 @@ class CustomListTests(TestCase):
         res_1 = clist_5 - clist_3
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([-2, -2, -2, 4, 5], res_1)
-        self.assertEqual([3, 4, 5], clist_3)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([-2, -2, -2, 4, 5], res_1))
+        self.assertEqual(True, compare_clist_list([3, 4, 5], clist_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         res_2 = clist_3 - clist_5
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([2, 2, 2, -4, -5], res_2)
-        self.assertEqual([3, 4, 5], clist_3)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([2, 2, 2, -4, -5], res_2))
+        self.assertEqual(True, compare_clist_list([3, 4, 5], clist_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         clist_3_1 = [1, 2, 3]
         res_3 = clist_3 - clist_3_1
 
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([2, 2, 2], res_3)
-        self.assertEqual([3, 4, 5], clist_3)
-        self.assertEqual([1, 2, 3], clist_3_1)
+        self.assertEqual(True, compare_clist_list([2, 2, 2], res_3))
+        self.assertEqual(True, compare_clist_list([3, 4, 5], clist_3))
+        self.assertEqual(True, compare_clist_list([1, 2, 3], clist_3_1))
 
     def test_sub_custom_list_and_list(self):
         clist_5 = CustomList([1, 2, 3, 4, 5])
@@ -149,24 +156,24 @@ class CustomListTests(TestCase):
         res_1 = clist_5 - list_3
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([-1, -1, -1, 4, 5], res_1)
+        self.assertEqual(True, compare_clist_list([-1, -1, -1, 4, 5], res_1))
         self.assertEqual([2, 3, 4], list_3)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         res_2 = clist_5 - list_6
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([-2, -1, 0, 3, 3, -3], res_2)
+        self.assertEqual(True, compare_clist_list([-2, -1, 0, 3, 3, -3], res_2))
         self.assertEqual([3, 3, 3, 1, 2, 3], list_6)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         list_5 = [5, 4, 3, 2, 1]
-        res_3 = clist_5 - list_6
+        res_3 = clist_5 - list_5
 
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([-4, -2, 0, 2, 4], res_3)
+        self.assertEqual(True, compare_clist_list([-4, -2, 0, 2, 4], res_3))
         self.assertEqual([5, 4, 3, 2, 1], list_5)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
     def test_rsub_custom_list_and_list(self):
         clist_5 = CustomList([1, 2, 3, 4, 5])
@@ -178,24 +185,24 @@ class CustomListTests(TestCase):
         res_1 = list_3 - clist_5
 
         self.assertEqual(CustomList, type(res_1))
-        self.assertEqual([1, 1, 1, -4, -5], res_1)
+        self.assertEqual(True, compare_clist_list([1, 1, 1, -4, -5], res_1))
         self.assertEqual([2, 3, 4], list_3)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         res_2 = list_6 - clist_5
 
         self.assertEqual(CustomList, type(res_2))
-        self.assertEqual([2, 1, 0, -3, -3, 3], res_2)
+        self.assertEqual(True, compare_clist_list([2, 1, 0, -3, -3, 3], res_2))
         self.assertEqual([3, 3, 3, 1, 2, 3], list_6)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
         list_5 = [8, 7, 6, 5, 4]
         res_3 = list_5 - clist_5
 
         self.assertEqual(CustomList, type(res_3))
-        self.assertEqual([7, 5, 3, 1, -1], res_3)
+        self.assertEqual(True, compare_clist_list([7, 5, 3, 1, -1], res_3))
         self.assertEqual([8, 7, 6, 5, 4], list_5)
-        self.assertEqual([1, 2, 3, 4, 5], clist_5)
+        self.assertEqual(True, compare_clist_list([1, 2, 3, 4, 5], clist_5))
 
     def test_eq(self):
         clist_5 = CustomList([1, 2, 3, 4, 5])
